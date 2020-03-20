9d1e1561b0d55d80264fe821d3d9fc0585cb91f7
Alexander Popov <alex.val.popov@gmail.com>
2020-03-20 16:31:31 +0200

    arranged_prob

--- src/clojure_katas/arranged_prob.clj
+++ src/clojure_katas/arranged_prob.clj
@@ -12,4 +12,8 @@
    example:
    If a box contains twenty-one coloured discs, composed of 15 blue discs and 6 red discs,
    and 2 discs were taken at random, it can be seen that the probability of taking 2 blue discs, P(BB) = (15/21)×(14/20) = 1/2."
-  [p, m, n])
+  [p, m, n]
+  (let [next-probability (if (= n 1)
+                           1
+                           (prob (dec p) (dec m) (dec n)))]
+    (* (/ m p) next-probability)))
